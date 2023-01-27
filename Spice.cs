5b379395daa19528fc09e6039c7f8caf17a0afe6
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-01-27 16:35:24 -0600

    Prototype what "hello world" is
    Reading Flutter hello world:
    
    https://docs.flutter.dev/get-started/flutter-for/xamarin-forms-devs

---
+++ Spice.cs
@@ -0,0 +1,37 @@
+﻿namespace spice;
+
+public class App : Application
+{
+    public App () => Main = new View {
+        Children = {
+            new Label {
+                Text = "Hello, Spice! 🌶",
+                Align = Align.Center,
+            }
+        }
+    };
+}
+
+public class Application
+{
+    public View? Main { get; set; }
+}
+
+public class View
+{
+    public IList<View> Children { get; set; } = new List<View>();
+}
+
+public class Label : View
+{
+    public string Text { get; set; } = "";
+
+    public Align Align { get; set; }
+}
+
+public enum Align
+{
+    Start,
+    Center,
+    End,
+}
