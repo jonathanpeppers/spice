f3a0dc7b2d028b7762c0d3111fe5f32c6fe62ecd
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-01-27 21:51:03 -0600

    Add Button

--- Spice.cs
+++ Spice.cs
@@ -2,14 +2,27 @@
 
 public class App : Application
 {
-    public App () => Main = new View {
-        Children = {
-            new Label {
-                Text = "Hello, Spice! 🌶",
-                Align = Align.Center,
+    public App ()
+    {
+        int count = 0;
+
+        var label = new Label {
+            Text = "Hello, Spice! 🌶",
+            Align = Align.Center,
+        };
+
+        var button = new Button {
+            Text = "Click Me",
+            Clicked = _ => label.Text = $"Times: {++count}"
+        };
+
+        Main = new View {
+            Children = {
+                label,
+                button,
             }
-        }
-    };
+        };
+    }
 }
 
 public class Application
@@ -19,14 +32,19 @@ public class Application
 
 public class View
 {
+    public Align Align { get; set; }
+
     public IList<View> Children { get; set; } = new List<View>();
 }
 
 public class Label : View
 {
     public string Text { get; set; } = "";
+}
 
-    public Align Align { get; set; }
+public class Button : Label
+{
+    public Action<Button> Clicked { get; set; } = delegate { };
 }
 
 public enum Align
