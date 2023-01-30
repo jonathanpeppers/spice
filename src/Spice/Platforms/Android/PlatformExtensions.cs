9b0248b3d6b3051e840eb6345ac1f5270afea7ff
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-01-30 16:09:52 -0600

    Create PlatformExtensions.cs

---
+++ src/Spice/Platforms/Android/PlatformExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Android.Util;
+
+namespace Spice;
+
+public static class PlatformExtensions
+{
+	static readonly Lazy<float> Density = new Lazy<float>(() => Platform.Context!.Resources!.DisplayMetrics!.Density);
+
+	public static int ToPixels(this int value) => (int)Math.Round(value * Density.Value);
+}
