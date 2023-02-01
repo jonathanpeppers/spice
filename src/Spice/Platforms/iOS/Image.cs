3d6989e04128223a587322568839c53571b7182a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-02-01 16:13:22 -0600

    [ios] implemented Image

---
+++ src/Spice/Platforms/iOS/Image.cs
@@ -0,0 +1,16 @@
+﻿namespace Spice;
+
+public partial class Image
+{
+	public static implicit operator UIImageView(Image image) => image.NativeView;
+
+	public Image() : base(_ => new UIImageView { AutoresizingMask = UIViewAutoresizing.None }) { }
+
+	public Image(CGRect rect) : base(_ => new UIImageView(rect) { AutoresizingMask = UIViewAutoresizing.None }) { }
+
+	public Image(Func<View, UIView> creator) : base(creator) { }
+
+	public new UIImageView NativeView => (UIImageView)_nativeView.Value;
+
+	partial void OnSourceChanged(string value) => NativeView.Image = UIImage.FromFile($"{value}.png");
+}
