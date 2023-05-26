c0767d9409c7c606055e1ae9385bcf58c9055e62
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-05-26 10:44:57 -0500

    [webview] basic scenarios working on Android
    Context: https://developer.android.com/develop/ui/views/layout/webapps/webview

---
+++ samples/Spice.Scenarios/Scenarios/WebViewScenario.cs
@@ -0,0 +1,41 @@
+﻿namespace Spice.Scenarios;
+
+/// <summary>
+/// Scenario navigating a WebView
+/// </summary>
+class WebViewScenario : StackView
+{
+	public WebViewScenario()
+	{
+		HorizontalAlign = Align.Stretch;
+		VerticalAlign = Align.Stretch;
+
+		var webView = new WebView
+		{
+			Source = "https://www.google.com",
+			VerticalAlign = Align.Stretch,
+			HorizontalAlign = Align.Stretch,
+		};
+
+		var back = new Button
+		{
+			Text = "<",
+			Clicked = _ => webView.Back(),
+		};
+		var forward = new Button
+		{
+			Text = ">",
+			Clicked = _ => webView.Forward(),
+		};
+
+		Add(new StackView
+		{
+			Orientation = Orientation.Horizontal,
+			Children =
+			{
+				back, forward,
+			}
+		});
+		Add(webView);
+	}
+}
---
+++ src/Spice/Platforms/Android/WebView.cs
@@ -0,0 +1,92 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Webkit;
+
+namespace Spice;
+
+public partial class WebView
+{
+	/// <summary>
+	/// Returns view.NativeView
+	/// </summary>
+	/// <param name="view">The Spice.WebView</param>
+	public static implicit operator Android.Webkit.WebView(WebView view) => view.NativeView;
+
+	static Android.Webkit.WebView Create(Context context)
+	{
+		var view = new Android.Webkit.WebView(context);
+		view.SetWebViewClient(new SpiceWebViewClient());
+		view.Settings.JavaScriptEnabled = true; // This is the default
+		return view;
+	}
+
+	/// <summary>
+	/// A "web view" for rendering HTML content on each platform.
+	/// </summary>
+	public WebView() : this(Platform.Context, Create) { }
+
+	/// <inheritdoc />
+	/// <param name="context">Option to pass the desired Context, otherwise Platform.Context is used</param>
+	public WebView(Context context) : this(context, Create) { }
+
+	/// <inheritdoc />
+	/// <param name="creator">Subclasses can pass in a Func to create a Android.Views.View</param>
+	protected WebView(Func<Context, Android.Views.View> creator) : this(Platform.Context, creator) { }
+
+	/// <inheritdoc />
+	/// <param name="context">Option to pass the desired Context, otherwise Platform.Context is used</param>
+	/// <param name="creator">Subclasses can pass in a Func to create a Android.Views.View</param>
+	protected WebView(Context context, Func<Context, Android.Views.View> creator) : base(context, creator) { }
+
+	/// <summary>
+	/// The underlying Android.WebKit.WebView
+	/// </summary>
+	public new Android.Webkit.WebView NativeView => (Android.Webkit.WebView)_nativeView.Value;
+
+	partial void OnSourceChanged(string value) => NativeView.LoadUrl(value);
+
+	partial void OnIsJavaScriptEnabledChanged(bool value) => NativeView.Settings.JavaScriptEnabled = value;
+
+	/// <summary>
+	/// Returns true if you can go back
+	/// </summary>
+	public bool CanGoBack => NativeView.CanGoBack();
+
+	/// <summary>
+	/// Navigates back if possible
+	/// </summary>
+	public void Back()
+	{
+		NativeView.GoBack();
+		OnPropertyChanged(nameof(CanGoBack));
+	}
+
+	/// <summary>
+	/// Returns true if you can go back
+	/// </summary>
+	public bool CanGoForward => NativeView.CanGoForward();
+
+	/// <summary>
+	/// Navigates forward if possible
+	/// </summary>
+	public void Forward()
+	{
+		NativeView.GoForward();
+		OnPropertyChanged(nameof(CanGoForward));
+	}
+
+	class SpiceWebViewClient : WebViewClient
+	{
+		public SpiceWebViewClient() { }
+
+		public SpiceWebViewClient(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) { }
+
+		public override bool ShouldOverrideUrlLoading(Android.Webkit.WebView? view, IWebResourceRequest? request)
+		{
+			// Return false for http/https to navigate to the URL within the WebView
+			// Otherwise it launches the default browser from the OS
+			var scheme = request?.Url?.Scheme;
+			return scheme != "http" && scheme != "https";
+		}
+	}
+}
