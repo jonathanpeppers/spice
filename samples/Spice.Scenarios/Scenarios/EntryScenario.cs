94884ed829c8e15fb9623155521f45abecd3adba
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-05-30 12:24:00 -0500

    [entry] basic scenario working on iOS

---
+++ samples/Spice.Scenarios/Scenarios/EntryScenario.cs
@@ -0,0 +1,11 @@
+﻿namespace Spice.Scenarios;
+
+class EntryScenario : StackView
+{
+	public EntryScenario()
+	{
+		Add(new Entry { Text = "jonathan.peppers@gmail.com" });
+		Add(new Entry { IsPassword = true });
+		Add(new Button { Text = "Login" });
+	}
+}
---
+++ src/Spice/Platforms/iOS/Entry.cs
@@ -0,0 +1,36 @@
+﻿namespace Spice;
+
+public partial class Entry
+{
+	/// <summary>
+	/// Returns entry.NativeView
+	/// </summary>
+	/// <param name="entry">The Spice.Entry</param>
+	public static implicit operator UITextField(Entry entry) => entry.NativeView;
+
+	/// <summary>
+	/// Represents text on screen. Set the Text property to a string value.
+	/// Android -> Android.Widget.EditText
+	/// iOS -> UIKit.UITextField
+	/// </summary>
+	public Entry() : base(_ => new UITextField { AutoresizingMask = UIViewAutoresizing.None, BorderStyle = UITextBorderStyle.RoundedRect }) { }
+
+	/// <inheritdoc />
+	/// <param name="frame">Pass the underlying view a frame</param>
+	public Entry(CGRect frame) : base(_ => new UITextField(frame) { AutoresizingMask = UIViewAutoresizing.None, BorderStyle = UITextBorderStyle.RoundedRect }) { }
+
+	/// <inheritdoc />
+	/// <param name="creator">Subclasses can pass in a Func to create a UIView</param>
+	protected Entry(Func<View, UIView> creator) : base(creator) { }
+
+	/// <summary>
+	/// The underlying UITextField
+	/// </summary>
+	public new UITextField NativeView => (UITextField)_nativeView.Value;
+
+	partial void OnTextChanged(string value) => NativeView.Text = value;
+
+	partial void OnTextColorChanged(Color? value) => NativeView.TextColor = value.ToUIColor();
+
+	partial void OnIsPasswordChanged(bool value) => NativeView.SecureTextEntry = value;
+}
