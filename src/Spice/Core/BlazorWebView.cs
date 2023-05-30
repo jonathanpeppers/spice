b930c51083f404055b663bdf1227f6359093bf41
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-05-30 15:47:38 -0500

    Added AndroidWebKitWebViewManager

---
+++ src/Spice/Core/BlazorWebView.cs
@@ -0,0 +1,11 @@
+﻿namespace Spice;
+
+/// <summary>
+/// A "web view" for rendering Blazor/.razor content on each platform.
+/// Android -> Android.Webkit.WebView
+/// iOS -> WebKit.WKWebView
+/// </summary>
+public partial class BlazorWebView : View
+{
+	internal const string AppHostAddress = "0.0.0.0";
+}
